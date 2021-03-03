9fc7d33cb7ed2733a23ca23ed6389c89917de27e
hernanrnieva <hnieva@fi.uba.ar>
2021-03-03 15:47:45 -0300

    fix: resetear juego ya no genera error cuando no hay nada dibujado

--- src/main/java/edu/fiuba/algo3/modelo/Dibujo.java
+++ src/main/java/edu/fiuba/algo3/modelo/Dibujo.java
@@ -40,7 +40,7 @@ public class Dibujo {
 
     public void reset() {
         ArrayList<Posicion2D> nuevoDibujo = new ArrayList<>();
-        support.firePropertyChange("ultimaPosicionPersonaje", this.posicionesDibujadas, null);
+        support.firePropertyChange( "ultimaPosicionPersonaje", this.posicionesDibujadas, null);
         this.posicionesDibujadas = nuevoDibujo;
     }
 }
