d90633e94f5438664dbe0149874c67671035fb59
hernanrnieva <hnieva@fi.uba.ar>
2021-03-08 19:58:19 -0300

    refactor para redimension de espacio cuando se agregan muchos bloques

--- src/main/java/edu/fiuba/algo3/vista/AgregadorBloques.java
+++ src/main/java/edu/fiuba/algo3/vista/AgregadorBloques.java
@@ -4,6 +4,7 @@ import edu.fiuba.algo3.controlador.*;
 import edu.fiuba.algo3.modelo.Bloque;
 import edu.fiuba.algo3.modelo.BloqueAgregable;
 import edu.fiuba.algo3.modelo.RecorridoNormal;
+import javafx.event.EventHandler;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
@@ -18,20 +19,45 @@ import java.util.Map;
 
 public class AgregadorBloques {
     private static ArrayList<BloqueAgregable> bloquesPersonalizados = new ArrayList<>();
+    private final ArrayList<String> nombreBloques = new ArrayList<>();
+    private ArrayList<EventHandler> controladores = new ArrayList<>();
+
     private PropertyChangeSupport support = new PropertyChangeSupport(this);
 
+    private AgregadorBloques(){
+        nombreBloques.add("draw-north.png");
+        nombreBloques.add("draw-south.png");
+        nombreBloques.add("draw-east.png");
+        nombreBloques.add("draw-west.png");
+        nombreBloques.add("bajar.png");
+        nombreBloques.add("subir.png");
+        nombreBloques.add("invertir.png");
+        nombreBloques.add("repeticion-doble.png");
+        nombreBloques.add("repeticion-triple.png");
+    }
+
     public void agregarBloques(Pane pane, Rectangle2D screenBounds, Scene scene, RecorridoNormal recorrido) {
-        pane.getChildren().add(new ImagenBloque("draw-north.png",   screenBounds, scene, new ControladorBloqueMoverHaciaArriba(recorrido)));
-        pane.getChildren().add(new ImagenBloque("draw-south.png",   screenBounds, scene, new ControladorBloqueMoverHaciaAbajo(recorrido)));
-        pane.getChildren().add(new ImagenBloque("draw-east.png",    screenBounds, scene, new ControladorBloqueMoverADerecha(recorrido)));
-        pane.getChildren().add(new ImagenBloque("draw-west.png",    screenBounds, scene, new ControladorBloqueMoverAIzquierda(recorrido)));
-        pane.getChildren().add(new ImagenBloque("bajar.png",        screenBounds, scene, new ControladorBloqueBajar(recorrido)));
-        pane.getChildren().add(new ImagenBloque("subir.png",        screenBounds, scene, new ControladorBloqueSubir(recorrido)));
-        pane.getChildren().add(new ImagenBloque("invertir.png",     screenBounds, scene, new ControladorBloqueInvertir(recorrido)));
-        pane.getChildren().add(new ImagenBloque("repeticion-doble.png",   screenBounds, scene, new ControladorBloqueRepeticionDoble(recorrido)));
-        pane.getChildren().add(new ImagenBloque("repeticion-triple.png",   screenBounds, scene, new ControladorBloqueRepeticionTriple(recorrido)));
+        inicializarControladores(recorrido);
+
+        for (int i = 0; i < nombreBloques.size(); i++){
+            pane.getChildren().add(new ImagenBloque(nombreBloques.get(i), screenBounds, scene, controladores.get(i)));
+        }
 
         bloquesPersonalizados.forEach(bloque -> pane.getChildren().add(new ImagenBloqueGuardado(bloque.toString(), screenBounds, scene, recorrido, bloque)));
+    }
+
+    private void inicializarControladores(RecorridoNormal recorrido) {
+        controladores = new ArrayList<EventHandler>();
+
+        controladores.add(new ControladorBloqueMoverHaciaArriba(recorrido));
+        controladores.add(new ControladorBloqueMoverHaciaAbajo(recorrido));
+        controladores.add(new ControladorBloqueMoverADerecha(recorrido));
+        controladores.add(new ControladorBloqueMoverAIzquierda(recorrido));
+        controladores.add(new ControladorBloqueBajar(recorrido));
+        controladores.add(new ControladorBloqueSubir(recorrido));
+        controladores.add(new ControladorBloqueInvertir(recorrido));
+        controladores.add(new ControladorBloqueRepeticionDoble(recorrido));
+        controladores.add(new ControladorBloqueRepeticionTriple(recorrido));
     }
 
     private static AgregadorBloques instance = new AgregadorBloques();
@@ -52,5 +78,9 @@ public class AgregadorBloques {
     public void limpiar() {
         bloquesPersonalizados = new ArrayList<>();
         support.firePropertyChange("bloquesPersonalizados", null, bloquesPersonalizados);
+    }
+
+    public int getCantidadDeBloques() {
+        return bloquesPersonalizados.size() + nombreBloques.size();
     }
 }
